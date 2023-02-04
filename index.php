5e7952027859f35618ca461bb19865364506137b
albertoBarl <alberto.barletta00@gmail.com>
2023-02-04 12:17:21 +0100

    added milestone 1, content e genpass

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <?php 
-    
+    function genPass($length){
+        $alpha="abcdefghijklmnopqrstuvwxyz123456789!?&%$<>^+-*/()[]{}@#_='";
+        $password="";
 
-    
+        while(strlen($password) < $length){
+            $randomPass=$alpha[rand(0, strlen($alpha)-1)];
+            $password .= $randomPass;
+        };
+        return $password;
+
+    };
+
+    // CONTROLLO DA EFFETTUARE SULLA VALIDITà DELLA PASSWORD
+    if(isset($_GET["generatedPassword"]) && $_GET["generatedPassword"] < 8 || $_GET["generatedPassword"] > 32 ){
+        $error="Non hai inserito un valore valido, necessari tra gli 8 e i 32 caratteri.";
+    } 
+    elseif(isset($_GET["generatedPassword"]) && $_GET["generatedPassword"] !== ""){
+        $password = genPass($_GET["generatedPassword"]);
+    };
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -15,12 +34,24 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
 <body class="bg-dark">
-    <form class="form-control" action="./index.php" method="get">
-        <label>Lunghezza password:</label>
-        <div>
-            <input name="generatedPassword" placeholder="Lunghezza password">
-            <button type="submit" class="btn btn-danger">GENERATE</button>
-        </div>
-    </form>
+    <div>
+        <?php if(isset($error)){ ?>
+            <div class="alert alert-danger">
+                <?php echo $error?>
+            </div>
+        <?php } ?>
+        <?php if(isset($password)){ ?>
+            <div class="alert alert-success">
+                La password generata è: <?php echo $password ?>
+            </div>
+        <?php } ?>
+        <form class="form-control" action="./index.php" method="GET">
+            <label>Lunghezza password:</label>
+            <div>
+                <input type="number" name="generatedPassword" placeholder="Lunghezza password">
+                <button type="submit" class="btn btn-danger">GENERATE</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
